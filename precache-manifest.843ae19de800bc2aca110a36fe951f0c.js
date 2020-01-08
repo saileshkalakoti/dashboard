1b3c9d7dcaee0c507dc3eeec174ac38bab7348a9
kalakotisaileshh <saileshkalakoti>
2020-01-08 23:18:07 +0530

    Updates

---
+++ precache-manifest.843ae19de800bc2aca110a36fe951f0c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a3f8c9851bbc608afcb106cfaa98269",
+    "url": "/dashboard/index.html"
+  },
+  {
+    "revision": "d007a72dca75ce19af9b",
+    "url": "/dashboard/static/css/2.d4b275d9.chunk.css"
+  },
+  {
+    "revision": "09eaa779f8f9d1c1214e",
+    "url": "/dashboard/static/css/main.a14c7694.chunk.css"
+  },
+  {
+    "revision": "d007a72dca75ce19af9b",
+    "url": "/dashboard/static/js/2.5182408f.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/dashboard/static/js/2.5182408f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "09eaa779f8f9d1c1214e",
+    "url": "/dashboard/static/js/main.6d4cc421.chunk.js"
+  },
+  {
+    "revision": "77b6691996b4b8adc885",
+    "url": "/dashboard/static/js/runtime-main.a0d495ff.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cd1dfb1679220bdd41c06dce3e998fc8.js"
+  "/dashboard/precache-manifest.843ae19de800bc2aca110a36fe951f0c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dashboard/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
